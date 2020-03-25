cfc52a93d680e78c913eeeb89e1c5381279e960f
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-03-24 17:01:33 -0700

    stuff on dragging

--- src/index.js
+++ src/index.js
@@ -1,15 +1,57 @@
-//started on the script
 
-document.addEventListener("DOMContentLoaded", function() {
-    const newPlayer = document.getElementById("createAccountButton")
-    const oldPlayer = document.getElementById("WHATEVERTHENAMEWAS")
+
+//document.addEventListener("DOMContentLoaded", function() {
+  console.log("hehe")
+ 
   
-    newPlayer.addEventListener("submit", function() {
-        createAccount()
-    })
-  
-  })
-  
-  function createAccount () {
-  fetch something
-  }
+const fill= document.querySelector('.fill');
+const empties= document.querySelectorAll('.empty');
+
+//event listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+//loop through empties
+//then run your function
+for(const empty of empties) {
+    empty.addEventListener('dragover', towerOver);
+    empty.addEventListener('dragenter', towerEnter);
+    empty.addEventListener('dragleave', towerLeave);
+    empty.addEventListener('drop', towerDrop)
+    
+    }
+function dragStart() {
+    this.className += ' hold';
+
+    setTimeout(()=>(this.className = 'invisible'), 0)
+console.log('start')
+}
+
+function dragEnd() {
+this.className = 'fill'
+console.log('end')
+}
+
+
+
+
+function towerOver (e) {
+    e.preventDefault();
+console.log('over')
+}
+function towerEnter (e) {
+    e.preventDefault();
+    this.className += ' hovered';
+    console.log('enter')
+}
+function towerLeave () {
+    this.className += ' empty';
+    console.log('leave')
+}
+function towerDrop () {
+    this.className += ' empty';
+    this.append(fill);
+    console.log('drop')
+}
+
+
+//});
